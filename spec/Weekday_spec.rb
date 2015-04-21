5ead24ba542052e05032e31abaadc3b886c88f59
Jocelyn Alsdorf & Chris Sefkin <student@epicodus.com>
2015-04-21 10:15:10 -0700

    Updated specs

---
+++ spec/Weekday_spec.rb
@@ -0,0 +1,7 @@
+require ('rspec')
+require ('Weekday')
+
+describe ('Time#Weekday') do
+  it ("gets date of year from user and stores in variable") do
+    expect ((2015-07-11).Weekday()).to(eq(2015-07-11))
+end
